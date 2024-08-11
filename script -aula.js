adf46b25051d90870f2153fa330288bf5191034d
Rafael Dantas <dantas85.ie@gmail.com>
2024-08-11 20:41:16 +0100

    selecting elements js

---
+++ script -aula.js
@@ -0,0 +1,37 @@
+const animals = document.getElementById('animals');
+console.log(animals);
+
+
+const gridSection = document.getElementsByClassName('grid-section');
+console.log(gridSection[1])
+
+const primeiraLi = document.querySelector('li');
+console.log(primeiraLi);
+
+const primeiraUl = document.querySelector('ul');
+console.log(primeiraUl);
+
+const linkInterno = document.querySelector('[href^="#"]');
+console.log(linkInterno);
+
+const animalsImg = document.querySelectorAll('.animals img');
+console.log(animalsImg);
+
+// HTMLCollection vs NodeList
+const gridSectionHTML = document.getElementsByClassName('grid-section');
+const gridSectionNode = document.querySelectorAll('.grid-section');
+
+console.log(gridSectionHTML);
+console.log(gridSectionNode);
+
+
+// Array-Like
+gridSectionNode.forEach(function(item,index){
+console.log(item);
+});
+
+const arrayGrid = Array.from(gridSectionHTML);
+
+arrayGrid.forEach(function(item) {
+  console.log(item);
+})
--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-const animals = document.getElementById('animals');
-console.log(animals);
+// Exercício
+// Retorne no console todas as imagens do site
+const listaImg = document.querySelectorAll('img');
+
+console.log(listaImg);
+
+// Retorne no console apenas as imagens que começaram com a palavra imagem
+const imagensAnimais = document.querySelectorAll('img[src^="img/imagem"]');
+
+console.log(imagensAnimais);
+
+// Selecione todos os links internos (onde o href começa com #)
+const linksInternos = document.querySelectorAll('[href^="#"]');
+
+console.log(linksInternos);
+
+// Selecione o primeiro h2 dentro de .animais-descricao
+const animais = document.querySelector('.description-animals');
+
+const h2Animais = animais.querySelector('h2');
+console.log(animais);
+console.log(h2Animais);
+
+// Selecione o último p do site
+const paragrafos = document.querySelectorAll('p');
+
+console.log(paragrafos[paragrafos.length - 1]);
 
 
-const gridSection = document.getElementsByClassName('grid-section');
-console.log(gridSection[1])
 
-const primeiraLi = document.querySelector('li');
-console.log(primeiraLi);
-
-const primeiraUl = document.querySelector('ul');
-console.log(primeiraUl);
-
-const linkInterno = document.querySelector('[href^="#"]');
-console.log(linkInterno);
-
-const animalsImg = document.querySelectorAll('.animals img');
-console.log(animalsImg);
 
