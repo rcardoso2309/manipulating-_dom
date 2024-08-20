9dce7894498c1a0017f85aa6b42d1e37f090926c
Rafael Dantas <dantas85.ie@gmail.com>
2024-08-20 23:10:51 +0100

    Traversing e Manipulação exercises

--- script-aula07.1.js
+++ script-aula07.1.js
@@ -27,5 +27,44 @@ console.log(lista.querySelector('li:last-child'));
 
 console.log(lista.childNodes);
 
+const animais = document.querySelector('.animals');
+const contato = document.querySelector('.contact');
+const titulo = contato.querySelector('.title');
+
+const mapa = contato.querySelector('.map')
+
+lista.appendChild(titulo);
 
 
+// contato.appendChild(lista); // move lista para o final de contato
+// contato.insertBefore(lista, titulo); // insere a lista antes de titulo
+// contato.removeChild(titulo); // remove titulo de contato
+// contato.replaceChild(lista, titulo); // substitui titulo por lista
+
+
+const novoh1 = document.createElement('h1');
+novoh1.innerText = 'Novo Titulo';
+novoh1.classList.add('titulo')
+
+mapa.appendChild(novoh1);
+
+console.log(novoh1);
+
+// Clone Elements
+
+// const titulo = document.querySelector('h1');
+// const titulo2 = document.querySelector('h1');
+// const novoTitulo = titulo;
+// // titulo, titulo2 e novoTitulo são iguais
+
+// const cloneTitulo = titulo.cloneNode(true);
+// const contato = document.querySelector('.contato');
+// contato.appendChild(cloneTitulo);
+
+const H1 = document.querySelector('H1');
+const faq = document.querySelector('.faq');
+
+const cloneH1 = H1.cloneNode(true);
+
+cloneH1.classList.add('azul');
+faq.appendChild(cloneH1);
