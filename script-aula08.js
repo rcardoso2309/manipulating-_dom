73be83d943102731b44f3cc03ee67f2f3c51a9b4
Rafael Dantas <dantas85.ie@gmail.com>
2024-08-22 00:30:05 +0100

    Tab Navigation

--- script-aula08.js
+++ script-aula08.js
@@ -1,15 +1,23 @@
-const tabMenu = document.querySelectorAll('js-tabmenu li');
-const tabContent = document.querySelectorAll('js-tabcontent section');
+// Tab Navigation
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
+tabContent[0].classList.add('ativo');
 
-function activeTab(index) {
-  tabContent.forEach((section) => {
-    section.classList.remove('ativo');
+if(tabMenu.length && tabContent.length) {
+  function activeTab(index) {
+    tabContent.forEach((section) => {
+      section.classList.remove('ativo');
+    });
+    tabContent[index].classList.add('ativo');
+  }
+
+  tabMenu.forEach((itemMenu,index) => {
+    itemMenu.addEventListener('click', () => {
+      activeTab(index)
+    })
   });
-  tabContent[index].classList.add('ativo');
 }
 
-tabMenu.forEach((itemMenu,index) => {
-  itemMenu.addEventListener('click', () => {
-    activeTab(index)
-  })
-});
+// Accordion List
+
+
