35b3bfccdc54b99ca0d726239590dc839597f150
Rafael Dantas <dantas85.ie@gmail.com>
2024-08-26 23:41:00 +0100

    Smooth Scroll Internal

--- script-aula08.js
+++ script-aula08.js
@@ -45,16 +45,31 @@ function initAccordion() {
 initAccordion();
 
 // Scroll Suave Link Interno
+function initScrollSuave() {
 
-const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
 
-function srollToSection(event) {
-  event.preventDefault();
-  const href = event.currentTarget;
-  console.log(href);
-  
+  function srollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+
+  section.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
+
+  // forma alternativa
+  // const topo = section.offsetTop;
+  // window.scrollTo ({
+  //   top: topo,
+  //   behavior: "smooth"
+  //   });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', srollToSection)
+  });
 }
+initScrollSuave();
 
-linksInternos.forEach((link) => {
-  link.addEventListener('click', srollToSection)
-});
