29f9b58cb473bad9a2b6c2ab3de6102ff4bc1356
Rafael Dantas <dantas85.ie@gmail.com>
2024-08-29 00:23:46 +0100

    smooth Scrolling animation

--- script-aula08.js
+++ script-aula08.js
@@ -75,18 +75,25 @@ initScrollSuave();
 
 // Animação ao Scroll
 
-const sections = document.querySelectorAll('.js-scroll');
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('.js-scroll');
+  if(sections.length) {
+  const windowMetade = window.innerHeight * 0.6;
 
-function animaScroll() {
-  sections.forEach((section) =>{
-    const sectionTop = section.getBoundingClientRect().top;
-    console.log(sectionTop);
-    if(sectionTop < 0) {
-      console.log('animar');
-      
-    }
-  })
+  function animaScroll() {
+    sections.forEach((section) =>{
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = (sectionTop - windowMetade) < 0;
+      if(isSectionVisible) 
+        section.classList.add('ativo');     
+      else
+        section.classList.remove('ativo');
+    })
+  }
+
+  animaScroll();
+
+  window.addEventListener('scroll', animaScroll);
+  }
 }
-
-window.addEventListener('scroll', animaScroll);
-
+initAnimacaoScroll();
